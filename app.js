1068624fd5099b2fbb63502681e53e89b5c10284
Ricardo Testore <ricardo.testore@gmail.com>
2016-11-04 15:26:14 -0300

    versión depurada

--- app.js
+++ app.js
@@ -2,43 +2,46 @@
  'use strict';
 
  angular.module('LunchCheck', [])
-
  .controller('LunchCheckController', LunchCheckController);
 
     LunchCheckController.$inject = ['$scope'];
 
     function LunchCheckController($scope) {
 
-
-      $scope.sayMessage = function() {
-            var message = countMeals();
-            return mesage;
-          };
-
-
-          function countMeals() {
-
+      $scope.countMeals =  function () {
             var numMeals = 0;
             var text = "";
             var num_of_meals = CalculateAmountMeals($scope.lunch_menu_model);
 
             switch (num_of_meals) {
               case 0:
-              text = "Please enter data first";
-              break;
+                text = "Please enter data first";
+                break;
               case 1:
               case 2:
               case 3:
-              text = "Enjoy!";
+                text = "Enjoy!";
+              break;
 
               default:
-              text = "Too Much!";
+                text = "Too Much!";
             };
+
+              $scope.message = text;
+
           };
 
           function CalculateAmountMeals(string) {
-            var arrayOfMeals = string.split(',');
-            var numOfMeals = arrayOfMeals.length;
+            var arrayOfMeals = [];
+            var numOfMeals   = 0;
+
+
+            if (!string)
+              numOfMeals = 0;
+            else {
+               arrayOfMeals = string.split(',');
+               numOfMeals = arrayOfMeals.length;
+             };
 
             return numOfMeals;
 
@@ -46,4 +49,4 @@
 
  }
 
-});
+})();
