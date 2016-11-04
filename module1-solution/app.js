27956ce08f1a1e4033d3c66d4efb134558cebad4
Ricardo Testore <ricardo.testore@gmail.com>
2016-11-04 16:50:21 -0300

    versión con adicionales

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -16,15 +16,21 @@
             switch (num_of_meals) {
               case 0:
                 text = "Please enter data first";
+                $scope.message_color = 'red';
+                $scope.border_color = 'red';
                 break;
               case 1:
               case 2:
               case 3:
                 text = "Enjoy!";
+                $scope.message_color = 'green';
+                $scope.border_color = 'green';
               break;
 
               default:
                 text = "Too Much!";
+                $scope.message_color = 'green';
+                $scope.border_color = 'green';
             };
 
               $scope.message = text;
@@ -40,13 +46,16 @@
               numOfMeals = 0;
             else {
                arrayOfMeals = string.split(',');
-               numOfMeals = arrayOfMeals.length;
+               var arrayCount = arrayOfMeals.length;
+               var i = 0;
+               for (i = 0; i < arrayCount; i++) {
+                   if (arrayOfMeals[i] != '' && arrayOfMeals[i]!= ' ' )
+                     numOfMeals += 1;
+               }
              };
-
             return numOfMeals;
-
           };
 
- }
+     }
 
 })();
