71284c0856284e7442ab6999ee26bbf1e51a0859
Ricardo Testore <ricardo.testore@gmail.com>
2017-01-20 16:40:07 -0300

    directives

--- module3-solution/app.js
+++ module3-solution/app.js
@@ -9,60 +9,91 @@
     
     NarrowItDownController.$inject = ['$scope', 'MenuSearchService'];
  
+    
 function FoundItems() {
   var ddo = {
-    templateUrl: 'foundItems.html'
+    templateUrl: 'foundItems.html',
+    scope: {
+      items: '<',
+      myTitle: 'Search Results:',
+      onRemove: '&'
+    },
+    controller: FoundItemsDirectiveController,
+    controllerAs: 'list',
+    bindToController: true
   };
 
   return ddo;
-}  
+}
+    
+function FoundItemsDirectiveController() {
+  var list = this;
+
+
+}
 
 function NarrowItDownController($scope, MenuSearchService) {
   var list = this;
 
   list.getMenuItems = function () {
     var searchTerm = $scope.search_term;
-    alert(searchTerm);
     var promise = MenuSearchService.getMatchedMenuItems(searchTerm);
-          
-    console.log('Promise: ', promise);
+    promise.then(function (response) {
+      console.log('la respuesta recibida por el controller es : ' , response.filteredItems , 'cantidad:' , response.filteredItems.length );
+      list.filteredItems = response.filteredItems;
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
   }
+  
+  list.removeItem = function (itemIndex) {
+    MenuSearchService.removeItem(itemIndex);
+  };
 }
 
 
 MenuSearchService.$inject = ['$http', 'ApiBasePath'];
 function MenuSearchService($http, ApiBasePath) {
   var service = this;
+  var foundItems = [];  
+  var filteredItems = [];
 
   service.getMatchedMenuItems = function (searchTerm) {
-    var foundItems = '';  
+
     var response_call = $http({
       method: "GET",
       url: (ApiBasePath),
     })
    .then (function (response) { 
       foundItems = response.data;
-             console.log('foundItems: ', foundItems);
       var menu_items = foundItems.menu_items;
-      console.log('menu_items', menu_items); 
-      var filteredItems = [];    
+      console.log('todos los items: ', menu_items.lenght, menu_items);   
+
       var i = 0;   
       for (i=0; i<menu_items.length; i++) {
           var elem = menu_items[i];
-          console.log('elem', elem);
           if (elem.description.indexOf(searchTerm) != -1 ) {
              filteredItems.push(elem);  
           }
       }   
-      console.log('filteredItems:', filteredItems);
-      return filteredItems ;
+      console.log('filteredItems: ' , filteredItems) 
+      response.filteredItems = filteredItems;
+      return response ;
 
    })
    .catch (function (error) {
       console.log('Error: ', error);
-   })
+   });
+      
+   return response_call;
          
  }
+  
+ service.removeItem = function (itemIndex) {
+     filteredItems.splice(itemIndex, 1);
+  };
+
 }
     
 })();
